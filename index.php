c4db5783455cba7ed390578a7faa023d8e52df8f
sabrina <sabourodrigues@hotmail.fr>
2021-03-15 09:57:14 +0100

    quête 1 php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+$titleFilmVo = 'Indiana Jones and the Last Crusade';
+echo $titleFilmVo ;
+$thisMovieIsSeen = true ;
+echo $thisMovieIsSeen ;
+$releaseYear = 1989 ;
+echo $releaseYear ;
+$imdbRate = 8.2 ;
+echo $imdbRate ;
